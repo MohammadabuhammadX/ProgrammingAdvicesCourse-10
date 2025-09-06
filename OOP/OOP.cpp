46654a1c531a4a187662b93e6b9dc8c8bc0239a1
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-09-06 23:54:58 +0300

    commit

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,9 +1,68 @@
-
-
 #include <iostream>
+#include<string>
+using namespace std;
+
+class Person {
+private:
+
+	int _userId = 1;
+
+	string _firstName;
+	string _lastName;
+
+public:
+
+	int getUserId() {
+
+		return _userId;
+
+	}
+
+	void setFirstName(string FirstName) {
+
+		_firstName = FirstName;
+
+	}
+
+	string firstName() {
+
+		return _firstName;
+
+	}
+
+	void setLastName(string LastName) {
+
+		_lastName = LastName;
+
+	}
+
+	string getLastName() {
+
+		return _lastName;
+
+	}
+
+	string getFullName() {
+
+		return _firstName + _lastName;
+
+	}
+
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	Person Person1 , Person2;
+	
+	Person1.setFirstName("Mohammad");
+	Person1.setLastName(" AbuhMammad");
+
+
+	cout << "User Id : " << Person1.getUserId() << endl;
+	cout << "First Name : " << Person1.firstName() << endl;
+	cout << "Last Name : " << Person1.getLastName() << endl;
+	cout << "Full Name : " << Person1.getFullName() << endl;
 }
 
